e96427598416ca47cf6055b25b09eb2a4019b436
Şahin Beşinci <ahin Beinci>
2016-09-26 05:30:41 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sahin/loginwithfacebok/User.java
@@ -0,0 +1,18 @@
+package com.example.sahin.loginwithfacebok;
+
+/**
+ * Created by sahin on 26.09.2016.
+ */
+public class User {
+
+
+    public String name;
+
+    public String email;
+
+    public String facebookID;
+
+    public String gender;
+
+
+}
